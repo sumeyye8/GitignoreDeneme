7d10169a32dd8e7d97b6457eb92468f8f6b2ac76
Sümeyye Geçici <sumeyyegecici1998@gmail.com>
2022-10-15 06:31:07 +0300

    asd

--- src/GitIgnore.java
+++ src/GitIgnore.java
@@ -1,5 +1,6 @@
 public class GitIgnore {
     public static void main(String[] args) {
         System.out.println("GİtIgnore Deneme 1");
+        System.out.println("GİtIgnore Deneme 2");
     }
 }
